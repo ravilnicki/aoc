280ab6049698342f6ae9a0e647bf4db96fc7a651
ravilnicki <ravilnicki@gmail.com>
2023-04-02 12:09:48 +0200

    Refactor Go solution
    Refactor Go solution for day 1 of 2015 Advent of Code.

--- 2015/01/solution.go
+++ 2015/01/solution.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	line, _ := reader.ReadString('\n')
-	line = strings.TrimSpace(line)
-	floor, basement := 0, 0
+	var line string
+	var floor, basement int
+	fmt.Scanln(&line)
 	for i, ch := range line {
 		if ch == '(' {
 			floor++
