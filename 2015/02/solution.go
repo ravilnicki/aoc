f4c2e47a6bc3b0125b744c6f2da71279135f4dc4
ravilnicki <ravilnicki@gmail.com>
2023-04-06 23:14:43 +0200

    Refactor solution in Go
    Refactor solution in Go for day 2 of 2015 Advent of Code.

--- 2015/02/solution.go
+++ 2015/02/solution.go
@@ -5,22 +5,16 @@ import (
 	"fmt"
 	"os"
 	"sort"
-	"strconv"
-	"strings"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	paper, ribbon := 0, 0
 	for scanner.Scan() {
-		dimensions := strings.Split(scanner.Text(), "x")
-		sides := make([]int, len(dimensions))
-		for i, s := range dimensions {
-			n, _ := strconv.Atoi(s)
-			sides[i] = n
-		}
-		sort.Ints(sides)
-		w, l, h := sides[0], sides[1], sides[2]
+		dims := make([]int, 3)
+		fmt.Sscanf(scanner.Text(), "%dx%dx%d", &dims[0], &dims[1], &dims[2])
+		sort.Ints(dims)
+		w, l, h := dims[0], dims[1], dims[2]
 		paper += 2*(w*l+l*h+w*h) + w*l
 		ribbon += 2*(w+l) + w*l*h
 	}
